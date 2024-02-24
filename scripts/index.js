f3971d73edc1e3d431290ca5341dc2047d325951
Yash Kadam <kadamyash.bz@gmail.com>
2024-02-24 17:29:43 +0530

    basic code running with js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 const date = 8;
+function getParams() {
+    const queryParams = new URLSearchParams(window.location.search);
+    return(queryParams.get("name"))
+}
+
+const le_name = getParams();
+
+le_name?document.getElementById("greeting").innerHTML = "Happy Women's Day "+ le_name:null;
